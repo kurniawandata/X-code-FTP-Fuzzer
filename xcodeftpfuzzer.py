5d5946ac2e3c87bf2bfcedd583a80eff8e128202
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2018-10-18 14:03:59 +0700

    Create xcodeftpfuzzer.py

---
+++ xcodeftpfuzzer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import socket
+import sys
+ 
+if len(sys.argv) != 6:
+    print "X-code FTP Fuzzer v0.1" 
+    print "Oleh Kurniawan. trainingxcode@gmail.com. xcode.or.id."
+    print "Cara penggunaan : ./fuzzerhttp.py <ip target> <port> <karakter> <jumlah karakter> <command ftp>"
+    sys.exit(1)
+ 
+ipaddress = sys.argv[1]
+port = int(sys.argv[2])
+karakter = sys.argv[3]
+paket = int(sys.argv[4])
+command = sys.argv[5]
+buffer = (karakter) * (paket)
+
+try:
+ print "  -----------------------"
+ print "  X-code FTP Fuzzer v0.1"
+ print "  Oleh Kurniawan"
+ print "  trainingxcode@gmail.com"
+ print "  xcode.or.id"
+ print "  -----------------------"
+ print "" 
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ s.connect((str(ipaddress),(port)))
+ print "  Kirim paket"
+ s.send("(command) "+buffer+"\r\n")
+ print s.recv(1024)
+ s.close()
+
+except:
+  print "  Tidak dapat terkoneksi ke FTP Server"
